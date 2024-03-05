580701860e23abd49ffb9fdb685e43bab76a1860
세은 <seeunbag30@gmail.com>
2024-03-05 14:50:02 +0900

    Docs: 개발 환경 설정

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tsconfigPaths()],
+
+  resolve: {
+    alias: [{ find: '@pages', replacement: './pages' }],
+  },
+
+  server: {
+    port: 3000,
+  },
+});
